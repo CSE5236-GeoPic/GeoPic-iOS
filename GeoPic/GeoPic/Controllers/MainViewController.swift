0122ad5b35056fdb35faa3b10aca1861cd260a67
Dave Becker <dmb43016@gmail.com>
2021-02-25 11:42:59 -0500

    Settings button & forced portrait orientation

--- GeoPic/GeoPic/Controllers/MainViewController.swift
+++ GeoPic/GeoPic/Controllers/MainViewController.swift
@@ -12,14 +12,20 @@ class MainViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet private var mapView: MKMapView!
     @IBOutlet private var cameraButton: UIButton!
+    @IBOutlet private var settingsButton: UIButton!
     
     var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set up map location
+        // Set up map
         mapView.showsUserLocation = true
+        // Remove apple maps logo
+        mapView.subviews[1].isHidden = true
+        // Remove "legal" button
+        mapView.subviews[2].isHidden = true
+        // Request location access
         self.locationManager.requestWhenInUseAuthorization()
         if CLLocationManager.locationServicesEnabled() {
             locationManager.delegate = self
@@ -32,6 +38,11 @@ class MainViewController: UIViewController, CLLocationManagerDelegate {
         cameraButton.imageView?.contentMode = .scaleAspectFit
         cameraButton.layer.masksToBounds = true
         cameraButton.layer.cornerRadius = cameraButton.frame.width/2
+        
+        // Set up settings button
+        settingsButton.imageView?.contentMode = .scaleAspectFit
+        settingsButton.layer.masksToBounds = true
+        settingsButton.layer.cornerRadius = settingsButton.frame.width/2
     }
     
     // Taken from: https://stackoverflow.com/questions/52564004/location-marker-not-displaying-swift-4

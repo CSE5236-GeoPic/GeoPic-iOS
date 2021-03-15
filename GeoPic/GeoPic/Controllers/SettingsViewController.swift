ad87cc1743929b72a0394eb9724d202a1fca47d4
johnchoi96 <johnchoi1003@icloud.com>
2021-03-15 09:24:32 -0400

    User deletion implemented

--- GeoPic/GeoPic/Controllers/SettingsViewController.swift
+++ GeoPic/GeoPic/Controllers/SettingsViewController.swift
@@ -13,7 +13,7 @@ class SettingsViewController: UIViewController {
     @IBOutlet weak var table: UITableView!
     
     // list of settings available
-    let settingsItems: [String] = ["Change Name", "Change Password", "Log Out"]
+    let settingsItems: [String] = ["Change Name", "Change Password", "Delete Account", "Log Out"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +26,17 @@ class SettingsViewController: UIViewController {
         table.register(SettingsTableViewCell.nib, forCellReuseIdentifier: SettingsTableViewCell.identifier)
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == K.Segues.settingsToChange {
+            let vc = segue.destination as! SettingsChangeViewController
+            vc.pageTitle = sender as? String
+            if (sender as! String) == "Change Name" {
+                vc.changeType = .name
+            } else {
+                vc.changeType = .password
+            }
+        }
+    }
 }
 
 // MARK: - Settings table view delegate and data source methods
@@ -58,31 +68,86 @@ extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
                 return
             }
             self.navigationController?.popToRootViewController(animated: true)
+        } else if settingsItems[indexPath.row] == "Delete Account" {
+            let actionAlert = UIAlertController(title: "Are you sure?", message: "This cannot be reverted", preferredStyle: .actionSheet)
+            let confirmAction = UIAlertAction(title: "Delete Account", style: .destructive) { (action) in
+                self.deleteAccount()
+            }
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            actionAlert.addAction(confirmAction)
+            actionAlert.addAction(cancelAction)
+            present(actionAlert, animated: true, completion: nil)
         } else {
             // make segue to the change view
             performSegue(withIdentifier: K.Segues.settingsToChange, sender: settingsItems[indexPath.row])
         }
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == K.Segues.settingsToChange {
-            let vc = segue.destination as! SettingsChangeViewController
-            vc.pageTitle = sender as? String
-            if (sender as! String) == "Change Name" {
-                vc.changeType = .name
-            } else {
-                vc.changeType = .password
-            }
+    /**
+     Performs the following steps and deletes the current user's account.
+     
+     1. First, it reauthenticates the current user with the password provided by the user.
+     2. If reauthentication is successful, the user's data on Firestore is deleted using `Auth.auth().currentUser?.uid`
+     3. If data deletion is successful, the entire user is deleted from Firebase Auth.
+     4. If user deletion is successful, alert message is displayed and view is popped back to the root.
+     */
+    private func deleteAccount() {
+        // first, confirm password
+        let passwordPrompt = UIAlertController(title: "Please type in your password", message: "", preferredStyle: .alert)
+        var passwordfield: UITextField = UITextField()
+        passwordPrompt.addTextField { (tf) in
+            tf.placeholder = "Password"
+            passwordfield = tf
         }
+        let deleteAction = UIAlertAction(title: "Delete", style: .default) { (action) in
+            let user = Auth.auth().currentUser
+            let credential = EmailAuthProvider.credential(withEmail: user!.email!, password: passwordfield.text!)
+            
+            user?.reauthenticate(with: credential, completion: { (authResult, error) in
+                if error != nil {
+                    // error happened
+                    self.displayAccountDeleteAlert(title: "Failed to delete account", message: "Please try again later")
+                } else {
+                    // if password is correct, delete current user's data from the Firestore db
+                    let db = Firestore.firestore()
+                    let userId = user?.uid
+                    db.collection("users").document(userId!).delete() { err in
+                        if err != nil {
+                            self.displayAccountDeleteAlert(title: "Failed to delete account", message: "Please try again later")
+                        } else {
+                            // delete user from Firebase Auth
+                            user?.delete(completion: { (error) in
+                                if error != nil {
+                                    self.displayAccountDeleteAlert(title: "Failed to delete account", message: "Please try again later")
+                                } else {
+                                    let confirmationAlert = UIAlertController(title: "Account deleted!", message: "", preferredStyle: .alert)
+                                    let action = UIAlertAction(title: "OK", style: .default) { (action) in
+                                        self.navigationController?.popToRootViewController(animated: true)
+                                    }
+                                    confirmationAlert.addAction(action)
+                                    self.present(confirmationAlert, animated: true, completion: nil)
+                                }
+                            })
+                        }
+                    }
+                }
+            })
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        passwordPrompt.addAction(deleteAction)
+        passwordPrompt.addAction(cancelAction)
+        present(passwordPrompt, animated: true, completion: nil)
     }
-}
-
-#warning("Debug purpose methods. Delete when ready to deploy")
-extension SettingsViewController {
     
-    private func displayAlert(action: String) {
-        let alert = UIAlertController(title: "Work In Progress", message: "Action of <\(action)> happens here", preferredStyle: .alert)
-        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+    /**
+     Displays the alert controller with passed in title and message.
+     The alert will contain the one `OK` button.
+     - Parameter title: title of the alert
+     - Parameter message: message of the alert
+     */
+    private func displayAccountDeleteAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
